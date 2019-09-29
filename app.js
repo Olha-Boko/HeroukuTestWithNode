4aa06a9c7e912ee05f38d341d04845801824ea18
Olga <olgenbox@gmail.com>
2019-09-28 23:47:47 -0400

    added index.html and contact.html

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const path = require('path'); //path lets as navigate to the file sistem/folder
+
 // Heroku assigns a port when it via the process (environmenr variables)
 //localy this will run @ port 3000; remotaly it'll run werever heroku tells it to run
 const port = process.env.PORT || 3000; // the || means "or"
 
 const app =express();
 
+//a forvard slash is the home route (same as index.html)
 app.get('/', (req, res) => {
-  res.send('hello world! here!');
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+  //this builds localhost:3000/views/index.html
 })
 
 app.get('/contact', (req, res) => {
-  res.send('on the contact page! here!');
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
 })
 
 app.get('/portfolio', (req, res) => {
